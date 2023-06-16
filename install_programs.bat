aab8cd1225c18fbeefb841f7bbd1510f030097c3
calebcano <74797760+calebcano@users.noreply.github.com>
2023-06-16 12:01:54 -0500

    Update install_programs.bat

--- install_programs.bat
+++ install_programs.bat
@@ -12,23 +12,23 @@ start /wait "Steam" "https://cdn.cloudflare.steamstatic.com/client/installer/Ste
 echo Steam installation completed.
 
 echo Installing Mullvad VPN...
-start /wait "Mullvad VPN" "https://mullvad.net/download-latest?platform=win"
+start /wait "Mullvad VPN" "https://mullvad.net/en/download/app/exe/latest"
 echo Mullvad VPN installation completed.
 
 echo Installing League of Legends...
-start /wait "League of Legends" "https://signup.na.leagueoflegends.com/en/signup/redownload"
+start /wait "League of Legends" "https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe"
 echo League of Legends installation completed.
 
 echo Installing Spotify...
-start /wait "Spotify" "https://www.spotify.com/download/windows/"
+start /wait "Spotify" "https://download.scdn.co/SpotifySetup.exe"
 echo Spotify installation completed.
 
 echo Installing OBS...
-start /wait "OBS" "https://obsproject.com/download"
+start /wait "OBS" "https://cdn-fastly.obsproject.com/downloads/OBS-Studio-29.1.2-Full-Installer-x64.exe"
 echo OBS installation completed.
 
 echo Installing Logitech G HUB...
-start /wait "Logitech G HUB" "https://www.logitechg.com/en-us/innovation/g-hub.html"
+start /wait "Logitech G HUB" "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe"
 echo Logitech G HUB installation completed.
 
 echo All installations completed.
